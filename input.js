99cc45c3b26b42137a5e0b96fc75fdf4d48637dd
charleenmperrier <charlee_perrier@hotmail.com>
2021-02-10 22:21:30 +0000

    added movement controlls

--- input.js
+++ input.js
@@ -1,11 +1,23 @@
+// Stores the active TCP connection object.
+let connection;
+
 const handleUserInput = function (key){
-  console.log(key)
+  // console.log(key)
   if (key === '\u0003') {
     process.exit();
-  }
+  } else if (key === 'w') {
+    connection.write('Move: up')
+  } else if (key === 'a') {
+    connection.write('Move: left')
+  } else if (key === 's') {
+    connection.write('Move: down')
+  } else if (key === 'd') {
+    connection.write('Move: right')
+  } 
 };
 
- const setupInput = function() {
+ const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
