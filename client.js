58df4cfb46d6f1ca8bd23928d935bcf365a210ef
charleenmperrier <charlee_perrier@hotmail.com>
2021-02-10 19:58:05 +0000

    refactored

---
+++ client.js
@@ -0,0 +1,20 @@
+/**
+ * Establishes connection with the game server
+ */
+const net = require('net');
+
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '10.0.2.15',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+  conn.on('data', (data) => {
+    console.log('Snake says: ', data);
+  });
+
+  return conn;
+}
+
+module.exports = { connect };
