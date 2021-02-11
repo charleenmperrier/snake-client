600221b78859e11245fabc2a0a5c7974cd676bdf
charleenmperrier <charlee_perrier@hotmail.com>
2021-02-11 00:20:07 +0000

    refactored

--- client.js
+++ client.js
@@ -2,11 +2,14 @@
  * Establishes connection with the game server
  */
 const net = require('net');
-
+const { IP, PORT } = require('./constants'); //added
+// console.log(IP)
 const connect = function() {
   const conn = net.createConnection({ 
-    host: '10.0.2.15',
-    port: 50541
+    
+    host: IP,
+    port: PORT
+    
   });
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
--- input.js
+++ input.js
@@ -5,23 +5,17 @@ const handleUserInput = function (key){
   // console.log(key)
   if (key === '\u0003') {
     process.exit();
-  } 
-  if (key === 'w') {
+  } else if (key === 'w') {
     connection.write('Move: up')
-  }
-  if (key === 'a') {
+  } else if (key === 'a') {
     connection.write('Move: left')
-  }
-  if (key === 's') {
+  } else if (key === 's') {
     connection.write('Move: down')
-  }
-  if (key === 'd') {
+  } else if (key === 'd') {
     connection.write('Move: right')
-  }
-  if (key === 'j') {
+  } else if (key === 'j') {
     connection.write('Say: Wuz')
-  }
-  if (key === 'k') {
+  } else if (key === 'k') {
     connection.write('Say: Up')
   }
 };
